66f0992fd3def1ffb3ea2cdf278b57cadca0980e
Yang Yifei <33801634+YangYifei666@users.noreply.github.com>
2018-12-17 09:33:02 +0800

    Update calculation.java
    SD

--- calculation.java
+++ calculation.java
@@ -3,40 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.group;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+package com.mycompany.issues19;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+
 /**
  *
  * @author lenovo
  */
-public class calculation extends Thread{
-    ArrayList number;
-    public calculation(ArrayList number){
-        this.number=number;
-    }
-    Iterator it = number.iterator();
-    double num=0;
-    int count=0;
-    double sum=0;
-    double mean=0;
-    double Variance=0;
-    double i=0;
-    double SD=0;
-
-    @Override
-    public void run() {
-        while(it.hasNext()){
-             num=(double) it.next();
-             count=number.size();
-             sum+=num;
-             mean=sum/count;
-             i=Math.pow((num-mean), 2);
-             Variance+=i;
-             SD=Math.sqrt(Variance/count);
-        }
-             System.out.println("Standard Deviation : " + SD );
+public class Main {
+    public static void main(String[] args) throws InterruptedException, ExecutionException{
+        int N=211;
+        ExecutorService executor=Executors.newCachedThreadPool();
+        Test test=new Test(N);
+        Future<Integer> result=executor.submit(test);
+        System.out.println("Value of N:"+N);
+        System.out.println("Total :"+result.get());
     }
 }
