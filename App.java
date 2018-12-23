3ef1dab69b666d88c7804b43a3bc47931643056a
peimei123 <37327475+peimei123@users.noreply.github.com>
2018-12-24 04:24:52 +0800

    Update App.java

--- App.java
+++ App.java
@@ -1,80 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 import java.io.File;
-
 import java.util.ArrayList;
-
 import java.util.List;
-
 import java.util.concurrent.Callable;
-
 import java.util.concurrent.ExecutionException;
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
+/**
+ *
+ * @author User
+ */
+public class RealTime {
 
+    public String choosefile() {
+        //Choose File to Read
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("PDF file");
+        //only choose excel file format
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("PDF", "pdf");
+        fileChooser.setFileFilter(filter);
+        fileChooser.showOpenDialog(null);
+        //File selectedfile = fileChooser.getCurrentDirectory();
+        String ret = new String();
+        ret = fileChooser.getSelectedFile().getPath();
+        return ret;
+    }
 
-public class App {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        //choose file
+        RealTime a = new RealTime();
+        File file = new File(a.choosefile());
 
-        // Create the list that will contain all the tasks
+        //Get ExecutorService from Executors utility class, thread pool size is 10
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
-        File file=new File("C:/Users/Dell/Desktop/example.txt");
+        //Create MyCallable instance
+        Callable<Integer> word = new CallableCountWords(file);
+        Callable<Integer> character = new CountCharacter(file);
+        
+        //submit Callable tasks to be executed by thread pool
+        Future<Integer> futureWord = executor.submit(word);
+        Future<Integer> futureChar = executor.submit(character);
 
-        List<FutureTask<Integer>> futuresWord = new ArrayList<>();
-
-        List<FutureTask<Integer>> futuresCh = new ArrayList<>();
-
-
-
-        // Create a task for the current String
-
-        FutureTask<Integer> futureword = new FutureTask<>(new CallableCountWords(file) {
-
-        });
-
-        FutureTask<Integer> futurech = new FutureTask<>(new CountCharacter(file) {
-
-        });
-
-            // Add the task to the list of tasks
-
-            futuresWord.add(futureword);
-
-            futuresCh.add(futurech);
-
-            // Provide the task to a new Thread and start it
-
-            new Thread(futureword).start();
-
-            new Thread(futurech).start();
-
-        int totalword = 0;
-
-        int totalchar = 0;
-
-        // Iterate over all the tasks
-
-        for (FutureTask<Integer> task : futuresWord) {
-
-        // Wait as long as it is needed to get the result of the current task
-
-            totalword += task.get();
-
-         }
-
-        // Print the total of vowels found
-
-         System.out.printf("Number of Word: %d%n", totalword);
-
-
-
-        for (FutureTask<Integer>task:futuresCh){
-
-            totalchar+=task.get();
-
-        }
-
-        System.out.println("Number of Character: "+totalchar);
+        System.out.println(futureWord.get());
+        System.out.println(futureChar.get());
+        //shut down the executor service now
+        executor.shutdown();
 
     }
 
